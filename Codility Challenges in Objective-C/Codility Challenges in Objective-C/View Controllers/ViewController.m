4989425a8b95b7e14933fab6b390e43e1e497ddd
Kevin Rafferty <audible59@yahoo.com>
2017-03-02 14:35:04 -0800

    * Added the Binary Gap solution.

--- Codility Challenges in Objective-C/Codility Challenges in Objective-C/View Controllers/ViewController.m
+++ Codility Challenges in Objective-C/Codility Challenges in Objective-C/View Controllers/ViewController.m
@@ -10,16 +10,59 @@
 
 @interface ViewController ()
 
+
+/**
+ This method will take a positive NSInteger that is within the range [1..2,147,483,647].
+
+ @param integer An NSInteger that will be converted to a binary representation.
+ @return The length of the longest binary gap. Zero will be returned if there is no binary gap.
+ */
+- (NSInteger)binaryGap:(NSInteger)integer;
+
 @end
 
 @implementation ViewController
 
+#pragma mark - View Life Cycle -
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSLog(@"Binary Gap is - %ld", (long)[self binaryGap:20]);
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
+
+#pragma mark - Codility Challenges -
+
+- (NSInteger)binaryGap:(NSInteger)integer {
+    NSInteger max = 0;
+    NSInteger gap = 0;
+    
+    // Starting from the right side of the binary we skip all zeroes until we come across a one
+    while ((integer % 2) == 0) {
+        integer /= 2;
+    }
+    
+    while (integer > 0) {
+        if ((integer % 2) == 0) {
+            gap++;
+        } else {
+            if (gap > max) {
+                max = gap;
+            }
+            
+            gap = 0;
+        }
+        
+        integer /= 2;
+    }
+    
+    return max;
+}
+
+#pragma mark - Helper Methods -
 
 @end
